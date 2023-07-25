e371849c3296d0faa0d9544d42a531f1eaa4dff5
Han Jim-hyo <jimuel.dimz05@gmail.com>
2023-07-25 21:15:05 +0800

    constant

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <cctype>
+#include <Windows.h>
 
-int main()
+const int WORD_LENGTH = 5;
+const int NOT_MATCH = 0;
+const int PARTIAL_MATCH = 1;
+const int MATCH = 2;
+
+int main(int, char **)
 {
     return 0;
 }
