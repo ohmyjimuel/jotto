ebdbfbaff8075110c2892c14c8ae17224bfc4571
Han Jim-hyo <jimuel.dimz05@gmail.com>
2023-07-27 14:11:22 +0800

    final na talaga to

--- main.cpp
+++ main.cpp
@@ -60,55 +60,65 @@ int main() {
     cout << "In this game, you will try to guess a word with varying number of letters, and limited attempts, based on the difficulty level.\n";
     cout << "In solving for the word, you will encounter special characters, such as ? and *, the ? means that the letter used is correct, but placed in a wrong position, while the * means that the letter is not included in the word at all.\n";
     cout << "The game will end once you have guess the correct word, or you have used all your attempts, and yet failed to solve the word.\n";
+    cout << "----------------------------------------------------\n";
     cout << "Choose the difficulty level:\n";
     cout << "1 - Easy (3-letter word, 8 attempts)\n";
     cout << "2 - Medium (4-letter word, 6 attempts)\n";
     cout << "3 - Hard (5-letter word, 4 attempts)\n";
+    cout << "Enter the difficulty: ";
 
     int difficulty;
     cin >> difficulty;
 
+    cout << "----------------------------------------------------\n";
+
     int wordLength;
     int attemptsLeft;
-    if (difficulty == 1) {
-        wordLength = 3;
-        attemptsLeft = 8;
-    } else if (difficulty == 2) {
-        wordLength = 4;
-        attemptsLeft = 6;
-    } else if (difficulty == 3) {
-        wordLength = 5;
-        attemptsLeft = 4;
-    } else {
-        cout << "Invalid difficulty level. Please choose between 1, 2, or 3.\n";
-        exit(EXIT_FAILURE);
+
+    switch (difficulty) {
+        case 1: {
+            wordLength = 3;
+            attemptsLeft = 8;
+            break;
+        } case 2: {
+            wordLength = 4;
+            attemptsLeft = 6;
+            break;
+        } case 3: {
+            wordLength = 5;
+            attemptsLeft = 4;
+            break;
+        } default: {
+            cout << "Invalid difficulty level. Please choose between 1, 2, or 3.\n";
+            exit(EXIT_FAILURE);
+        }
     }
 
     // Get the random word of the specified length for the player to guess
     string targetWord = getRandomWord(wordLength);
+    
+    cout << "Target word: " << targetWord << endl; //Debugger
 
     // Main game loop
-    while (attemptsLeft > 0) {
+    for (int ctr = 1; ctr <= attemptsLeft; ctr++) {
         string guess;
-        cout << "Enter your guess (attempt " << 9 - attemptsLeft << "): ";
+        cout << "Enter your guess (attempt " << ctr << "): ";
         cin >> guess;
 
         // Convert the guess to lowercase
         transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
 
         if (isMatch(targetWord, guess)) {
-           cout << "Congratulations! You guessed the word: " << targetWord << endl;
+            cout << "Congratulations! You guessed the word: " << targetWord << endl;
             break;
         } else {
             string feedback = getFeedback(targetWord, guess);
             cout << "Feedback: " << feedback << endl;
-            --attemptsLeft;
         }
-    }
 
-    if (attemptsLeft == 0) {
-        cout << "Sorry, you have used all your attempts. The word was: " << targetWord << std::endl;
+        if (ctr == attemptsLeft)
+            cout << "Sorry, you have used all your attempts. The word was: " << targetWord << std::endl;
     }
-
+    
     return 0;
 }
