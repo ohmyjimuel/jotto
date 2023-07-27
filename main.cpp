43eb60716cfc18e1b947d413b62da89e064e2491
Han Jim-hyo <jimuel.dimz05@gmail.com>
2023-07-27 13:30:53 +0800

    FINAL

--- main.cpp
+++ main.cpp
@@ -1,67 +1,114 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #include <algorithm>
-#include <Windows.h>
 
-const int WORD_LENGTH = 5;
-const int NOT_MATCHED = 0;
-const int PARTIAL_MATCHED = 1;
-const int MATCHED = 2;
-const int NUMBER_OF_GUESSES = 6;
+using namespace std;
 
-void toUpperCase(std::string &input)
-{
-    std::transform(First: input.begin(), Last: input.end(), Dest: input.begin(), Func: [](unsigned char c)
-                    { return std::toupper(C:c); });
-}
-
-std::string getRandomWord()
-{
-    // TODO fetch random word from API (e.g. WordsAPI)
-    return "ready";
-}
-
-bool isValidWord(std::string word)
-{
-    //TODO check word correctness from API (e.g. WordsAPI)
-    return word.length() == WORD_LENGTH && word.find_first_not_of(Ptr:"ABCDEFGHIJKLMNOPQRSTUVWXYZ") == std::string::npos;
-}
-
-void markMatches(std::vector<std::vector<int>> &matches, int currentGuessIndex, std::string targetWord, std::string guessWord)
-{
-    for (int i = 0; i < guessWord.length(); i++)
-    {
-        matches[currentGuessIndex][i] = NOT_MATCHED;
+// Function to check if the guessed word matches the target word
+bool isMatch(const string target, const string guess) {
+    if (target.length() != guess.length()) {
+        return false;
     }
 
-    for (int i = 0; i < guessWord.length(); i++)
-    {
-        for (int j = 0; j < targetWord.length(); j++)
-        {
-            if(guessword[i] == targetWord[j])
-            {
-                if (i == j)
-                {
-                    matches[currentGuessIndex][i] = MATCHED;
-                    break;
-                }
-                else
-                {
-                matches[currentGuessIndex][i] = PARTIAL_MATCHED;
-                }
-            }   
-        }
-    }
-}
-
-bool isAllMatched(std::string targetWord, std::string guessWord)
-{
-    for (int i = 0; i < targetWord.length(); i++)
-    {
-        if(targetWord[i] != guessWord[i])
+    for (size_t i = 0; i < target.length(); ++i) {
+        if (target[i] != guess[i]) {
             return false;
+        }
     }
 
     return true;
+}
+
+// Function to get the feedback for the guessed word
+string getFeedback(const string target, const string guess) {
+    string feedback;
+    for (size_t i = 0; i < target.length(); ++i) {
+        if (target[i] == guess[i]) {
+            feedback += target[i];
+        } else if (target.find(guess[i]) != string::npos) {
+            feedback += "?";
+        } else {
+            feedback += "*";
+        }
+    }
+
+    return feedback;
+}
+
+// Function to get a random word of the specified length
+string getRandomWord(int length) {
+    vector<string> words;
+    
+    // Add words of the specified length to the vector
+    if (length == 3) {
+        words = { "bat", "cat", "dog", "fan", "hat", "car" };
+    } else if (length == 4) {
+        words = { "frog", "jump", "cake", "love", "rock", "fire" };
+    } else if (length == 5) {
+        words = { "apple", "lemon", "happy", "world", "music", "beach" };
+    }
+    
+    srand(static_cast<unsigned>(time(nullptr)));
+    random_shuffle(words.begin(), words.end());
+    return words[0];
+}
+
+int main() {
+    cout << "Welcome to Jotto!\n";
+    cout << "How to Play:\n";
+    cout << "In this game, you will try to guess a word with varying number of letters, and limited attempts, based on the difficulty level.\n";
+    cout << "In solving for the word, you will encounter special characters, such as ? and *, the question mark implies that the letter used is correct, but placed in a wrong position, while the asterick means that the letter is not included in the word at all.\n";
+    cout << "The game will end once you have guess the correct word, or you have used all your attempts, and yet failed to solve the word.\n";
+    cout << "Choose the difficulty level:\n";
+    cout << "1 - Easy (3-letter word, 8 attempts)\n";
+    cout << "2 - Medium (4-letter word, 6 attempts)\n";
+    cout << "3 - Hard (5-letter word, 4 attempts)\n";
+
+    int difficulty;
+    cin >> difficulty;
+
+    int wordLength;
+    int attemptsLeft;
+    if (difficulty == 1) {
+        wordLength = 3;
+        attemptsLeft = 8;
+    } else if (difficulty == 2) {
+        wordLength = 4;
+        attemptsLeft = 6;
+    } else if (difficulty == 3) {
+        wordLength = 5;
+        attemptsLeft = 4;
+    } else {
+        cout << "Invalid difficulty level. Please choose between 1, 2, or 3.\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // Get the random word of the specified length for the player to guess
+    string targetWord = getRandomWord(wordLength);
+
+    // Main game loop
+    while (attemptsLeft > 0) {
+        string guess;
+        cout << "Enter your guess (attempt " << 9 - attemptsLeft << "): ";
+        cin >> guess;
+
+        // Convert the guess to lowercase
+        transform(guess.begin(), guess.end(), guess.begin(), ::tolower);
+
+        if (isMatch(targetWord, guess)) {
+           cout << "Congratulations! You guessed the word: " << targetWord << endl;
+            break;
+        } else {
+            string feedback = getFeedback(targetWord, guess);
+            cout << "Feedback: " << feedback << endl;
+            --attemptsLeft;
+        }
+    }
+
+    if (attemptsLeft == 0) {
+        cout << "Sorry, you have used all your attempts. The word was: " << targetWord << std::endl;
+    }
+
+    return 0;
 }
